9f528b4601b6f292f862cc116d73fa31f03be280
Fausto G. Tommasi <mmasito123@yahoo.com>
2016-09-07 09:57:04 -0500

    Started tokenization logic

--- slush.c
+++ slush.c
@@ -1,10 +1,28 @@
 #include <unistd.h>
 
 #define BUFFSIZE 200
+
+char** tokenize(char* string){
+  char** tokenized_string;
+  int end = 0;
+  int beginning = 0;
+  int i = 0;
+  while (string[i] != '\0'){
+   
+     if(string[i] == ' ' || string[i] == '(' || string[i] == ')'){
+       
+     }
+     i++;
+     end = i;
+  }
+  return tokenized_string;
+}
+
 int main(int argc, char** argv){
   while(1){
     char* buf;
     int read_result = read(STDIN_FILENO,buf,BUFFSIZE);
+    char** tokenize(buf);
     int write_result = write(STDOUT_FILENO,buf,BUFFSIZE);
   }
 
